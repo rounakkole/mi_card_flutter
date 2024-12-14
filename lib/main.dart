4380077f967da7a2f1e93ecfb39415cfb6502e54
rounak <rounakkole@gmail.com>
2024-12-15 00:31:36 +0530

    added: column, container

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
                 CircleAvatar(
                   radius: 50,
                   backgroundImage: AssetImage('images/location.png'),
+                  backgroundColor: Colors.teal,
                 ),
                 Text(
                   'User Name',
@@ -25,10 +26,63 @@ class MyApp extends StatelessWidget {
                     fontSize: 30,
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
-
                   ),
-
                 ),
+                Text(
+                  'profile header',
+                  style: TextStyle(
+                  fontSize: 20,
+                  letterSpacing: 2.5,
+                  color: Colors.white70,
+                ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: Row(
+                    children: [
+                      Icon(
+                          Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        '1234567890',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.teal,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 20,
+                        backgroundImage: AssetImage('images/email.png'),
+                        backgroundColor: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        'username@email.com',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.teal,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
               ],
             )
         )
